fabded47e645e25f17fca353e1f051f0b6d735e6
sohaib.hassan@mergestack.com <ishfaq.ahmed@mergestack.com>
2024-11-19 16:46:31 +0500

    Add dropdown for filtering by state in LeftSidebar component

--- src/components/common/leftSidebar/LeftSidebar.jsx
+++ src/components/common/leftSidebar/LeftSidebar.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LeftSidebar = () => {
+  const [showStatesDropdown, setShowStatesDropdown] = useState(false);
+
+  const [dropdownButtonText, setDropdownButtonText] = useState("Select State");
+
+  const handleStatesDropdown = () => {
+    setShowStatesDropdown(!showStatesDropdown);
+  };
+
+  const handleDropdownButtonText = (event) => {
+    const text = event.target.innerText;
+    setDropdownButtonText(text);
+    handleStatesDropdown();
+  };
+
   return (
     <div className="hidden lg:flex lg:col-span-3 pt-4 lg:sticky top-40 flex-col gap-4 h-fit">
       <span className="font-bold ml-3">Search</span>
@@ -10,16 +24,96 @@ const LeftSidebar = () => {
         placeholder="Search Blog"
       />
       <span className="font-bold ml-3">Filter By State</span>
-      <input
-        className="py-4 px-7 border border-gray-200 rounded-md"
-        type="text"
-        placeholder="Search Blog"
-      />
+      <div className="relative">
+        <button
+          onClick={handleStatesDropdown}
+          className="w-full flex items-center justify-between py-4 px-7 border border-gray-200 rounded-md"
+        >
+          <span>{dropdownButtonText}</span>
+          <svg
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            className={`${showStatesDropdown ? "hidden" : "block"}`}
+          >
+            <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+          </svg>
+          <svg
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            className={`${showStatesDropdown ? "block" : "hidden"}`}
+          >
+            <path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z" />
+          </svg>
+        </button>
+        <ul
+          className={`${
+            showStatesDropdown ? "block" : "hidden"
+          } absolute pt-3 border w-full bg-white border-gray-200`}
+        >
+          <li
+            onClick={handleDropdownButtonText}
+            className="cursor-pointer pb-2 px-5 transition-all duration-200 hover:scale-105"
+          >
+            All States
+          </li>
+          <li
+            onClick={handleDropdownButtonText}
+            className="cursor-pointer pb-2 px-5 transition-all duration-200 hover:scale-105"
+          >
+            West Virginia
+          </li>
+          <li
+            onClick={handleDropdownButtonText}
+            className="cursor-pointer pb-2 px-5 transition-all duration-200 hover:scale-105"
+          >
+            Michigan
+          </li>
+          <li
+            onClick={handleDropdownButtonText}
+            className="cursor-pointer pb-2 px-5 transition-all duration-200 hover:scale-105"
+          >
+            Ohio
+          </li>
+          <li
+            onClick={handleDropdownButtonText}
+            className="cursor-pointer pb-2 px-5 transition-all duration-200 hover:scale-105"
+          >
+            Arizona
+          </li>
+          <li
+            onClick={handleDropdownButtonText}
+            className="cursor-pointer pb-2 px-5 transition-all duration-200 hover:scale-105"
+          >
+            North Dakota
+          </li>
+          <li
+            onClick={handleDropdownButtonText}
+            className="cursor-pointer pb-2 px-5 transition-all duration-200 hover:scale-105"
+          >
+            South Carolina
+          </li>
+          <li
+            onClick={handleDropdownButtonText}
+            className="cursor-pointer pb-2 px-5 transition-all duration-200 hover:scale-105"
+          >
+            Utah
+          </li>
+          <li
+            onClick={handleDropdownButtonText}
+            className="cursor-pointer pb-2 px-5 transition-all duration-200 hover:scale-105"
+          >
+            New Jersey
+          </li>
+        </ul>
+      </div>
       <span className="font-bold ml-3">Share</span>
       <div className="flex flex-col gap-4">
         <button className="flex justify-start items-center hover:bg-[#1877F2] hover:text-white transition-all duration-200 border border-gray-200 rounded-md py-4 px-7">
           <svg
-            class="mr-6 inline"
             width="24"
             height="24"
             viewBox="0 0 24 24"
